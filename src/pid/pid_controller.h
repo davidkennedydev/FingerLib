32d4e907049f9e01c27a0b71aa3724bf1beeaafd
David Kennedy S. Araújo <software@davidkennedy.dev>
2025-07-09 19:59:25 -0300

    Code format

--- src/pid/pid_controller.h
+++ src/pid/pid_controller.h
@@ -1,86 +1,79 @@
 /*
-* pid_controller.h
-*
-* Created: 29/09/2016 13:48:12
-* Author: Olly McBride
-*
-* This work is licensed under the Creative Commons Attribution 4.0 International License.
-* To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/.
-*
-*/
-
+ * pid_controller.h
+ *
+ * Created: 29/09/2016 13:48:12
+ * Author: Olly McBride
+ *
+ * This work is licensed under the Creative Commons Attribution 4.0
+ * International License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by/4.0/.
+ *
+ */
 
 #ifndef PID_CONTROLLER_H_
 #define PID_CONTROLLER_H_
 
-#include <Arduino.h>
 #include "../timers/timer_and_delay.h"
+#include <Arduino.h>
 
+#define DEFAULT_LIMIT_MAX 255
+#define DEFAULT_LIMIT_MIN -255
 
-#define DEFAULT_LIMIT_MAX	255
-#define DEFAULT_LIMIT_MIN	-255
+// #define DEFAULT_GAIN_P		1.9
+// #define DEFAULT_GAIN_P		1.2
+// #define DEFAULT_GAIN_I		0
+// #define DEFAULT_GAIN_D		0
 
-//#define DEFAULT_GAIN_P		1.9
-//#define DEFAULT_GAIN_P		1.2
-//#define DEFAULT_GAIN_I		0
-//#define DEFAULT_GAIN_D		0
+// #define DEFAULT_GAIN_P		5
+// #define DEFAULT_GAIN_I		0.001
+// #define DEFAULT_GAIN_D		0
 
-//#define DEFAULT_GAIN_P		5
-//#define DEFAULT_GAIN_I		0.001
-//#define DEFAULT_GAIN_D		0
+// #define DEFAULT_GAIN_P		3
+// #define DEFAULT_GAIN_I		0.0013
+// #define DEFAULT_GAIN_D		0.1
 
-//#define DEFAULT_GAIN_P		3
-//#define DEFAULT_GAIN_I		0.0013
-//#define DEFAULT_GAIN_D		0.1
+// #define DEFAULT_GAIN_P		2.5
+// #define DEFAULT_GAIN_I		0.0003
+// #define DEFAULT_GAIN_D		0.1
 
-//#define DEFAULT_GAIN_P		2.5
-//#define DEFAULT_GAIN_I		0.0003
-//#define DEFAULT_GAIN_D		0.1
+#define DEFAULT_GAIN_P 2
+#define DEFAULT_GAIN_I 0
+#define DEFAULT_GAIN_D 10
 
-#define DEFAULT_GAIN_P		2
-#define DEFAULT_GAIN_I		0
-#define DEFAULT_GAIN_D		10
+class PID_CONTROLLER {
+public:
+  PID_CONTROLLER();
 
+  void enable(void);    // enable the PID controller (default)
+  void disable(void);   // disable the PID controller
+  void enable(bool en); // set the PID controller to be enabled/disabled
+  void reset(void);
 
+  double run(double targ, double curr); // run the PID computation
 
-class PID_CONTROLLER
-{
-	public:
-		PID_CONTROLLER();
+  void setLimits(double min, double max);   // set the output value limits
+  void getLimits(double *min, double *max); // get the output value limits
+  void setRampRate(double ramp); // set the maximum change in output limit
 
-		void enable(void);								// enable the PID controller (default)
-		void disable(void);								// disable the PID controller
-		void enable(bool en);							// set the PID controller to be enabled/disabled
-		void reset(void);
+  void setGains(float Kp, float Ki, float Kd);    // set the controller gains
+  void getGains(float *Kp, float *Ki, float *Kd); // get the controller gains
 
-		double run(double targ, double curr);			// run the PID computation
+private:
+  bool _en; // enable/disable flag
 
-		void setLimits(double min, double max);			// set the output value limits					
-		void getLimits(double *min, double *max);		// get the output value limits
-		void setRampRate(double ramp);					// set the maximum change in output limit
+  US_NB_TIMER _sampleTimer; // duration of sample in us
 
-		void setGains(float Kp, float Ki, float Kd);	// set the controller gains
-		void getGains(float *Kp, float *Ki, float *Kd);	// get the controller gains
+  double _min, _max; // output limits
+  double _rampLim;   // ramp rate limit
 
-	private:
-		bool _en;										// enable/disable flag
+  double _prevOutput; // previous output (used
 
-		US_NB_TIMER _sampleTimer;						// duration of sample in us
+  float _Kp, _Ki, _Kd; // PID controller gains
 
-		double _min, _max;								// output limits
-		double _rampLim;								// ramp rate limit
+  double _dInput; // change in input over time
+  double _prev;   // previous input
 
-		double _prevOutput;								// previous output (used 
-
-		float _Kp, _Ki, _Kd;							// PID controller gains
-
-		double _dInput;									// change in input over time 
-		double _prev;									// previous input
-
-		double _integral;								// integral value
-
+  double _integral; // integral value
 };
-
-
 
 #endif /* PID_CONTROLLER_H_ */
